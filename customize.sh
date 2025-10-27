037f4334249fbf2a4f16ec5a5db664cf0070ffb0
Jefino9488 <jefinojacob9488@gmail.com>
2025-10-27 19:15:56 +0530

    feat: Configure FrameworkPatcherModule for Framework Patcher V2
    - Clean up unnecessary files (README, LICENSE, changelog, etc.)
    - Configure module.prop with Framework Patcher V2 settings and compatibility
    - Improve customize.sh with specific framework file replacements
    - Add KSU and SUFS storage compatibility fixes
    - Remove unused directories (addon, zygisk)
    - Optimize for Magisk, KernelSU, and SUFS compatibility
    
    BREAKING CHANGE: Module now configured specifically for Framework Patcher V2

--- customize.sh
+++ customize.sh
@@ -1,64 +1,50 @@
 ##########################################################################################
 #
-# MMT Extended Config Script
+# Framework Patcher V2 Config Script
 #
 ##########################################################################################
 
 ##########################################################################################
-# Config Flags
+# Framework Files Replacement
 ##########################################################################################
 
-# Uncomment and change 'MINAPI' and 'MAXAPI' to the minimum and maximum android version for your mod
-# Uncomment DYNLIB if you want libs installed to vendor for oreo+ and system for anything older
-# Uncomment PARTOVER if you have a workaround in place for extra partitions in regular magisk install (can mount them yourself - you will need to do this each boot as well). If unsure, keep commented
-# Uncomment PARTITIONS and list additional partitions you will be modifying (other than system and vendor), for example: PARTITIONS="/odm /product /system_ext"
-#MINAPI=21
-#MAXAPI=25
-#DYNLIB=true
-#PARTOVER=true
-#PARTITIONS=""
-
-##########################################################################################
-# Replace list
-##########################################################################################
-
-# List all directories you want to directly replace in the system
-# Check the documentations for more info why you would need this
-
-# Construct your list in the following format
-# This is an example
-REPLACE_EXAMPLE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
-# Construct your own list here
 REPLACE="
+/system/framework/framework.jar
+/system/framework/services.jar
+/system/system_ext/framework/miui-services.jar
 "
+
+##########################################################################################
+# Storage Compatibility for KSU and SUFS
+##########################################################################################
+
+# Ensure proper storage paths for different root methods
+# This handles cases where MODPATH might not be set correctly
+if [ -z "$MODPATH" ]; then
+  if [ "$KSU" = "true" ] || [ "$SUFS" = "true" ]; then
+    MODPATH="/data/adb/modules/mod_frameworks"
+  else
+    MODPATH="/data/adb/modules/mod_frameworks"
+  fi
+fi
+
+# Additional compatibility for different storage locations
+if [ ! -d "$MODPATH" ]; then
+  if [ -d "/data/adb/modules_update/mod_frameworks" ]; then
+    MODPATH="/data/adb/modules_update/mod_frameworks"
+  elif [ -d "/data/modules/mod_frameworks" ]; then
+    MODPATH="/data/modules/mod_frameworks"
+  fi
+fi
 
 ##########################################################################################
 # Permissions
 ##########################################################################################
 
 set_permissions() {
-  : # Remove this if adding to this function
-
-  # Note that all files/folders in magisk module directory have the $MODPATH prefix - keep this prefix on all of your files/folders
-  # Some examples:
-  
-  # For directories (includes files in them):
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm_recursive $MODPATH/system/lib 0 0 0755 0644
-  # set_perm_recursive $MODPATH/system/vendor/lib/soundfx 0 0 0755 0644
-
-  # For files (not in directories taken care of above)
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  
-  # set_perm $MODPATH/system/lib/libart.so 0 0 0644
-  # set_perm /data/local/tmp/file.txt 0 0 644
+  # Set proper permissions for framework files
+  set_perm_recursive $MODPATH/system/framework 0 0 0755 0644 u:object_r:system_file:s0
+  set_perm_recursive $MODPATH/system/system_ext/framework 0 0 0755 0644 u:object_r:system_file:s0
 }
 
 ##########################################################################################
